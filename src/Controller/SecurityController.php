8ff6e8334280b2cc0e515b81987f7596a5689123
Kerkar <kerkar.kevin@gmail.com>
2024-12-03 17:48:33 +0100

    Google authentictor implemented

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\AccountType;
 use Doctrine\ORM\EntityManagerInterface;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
 use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -16,9 +24,11 @@ class SecurityController extends AbstractController
 {
 
     private $googleAuthenticator;
+    private $projectDir;
 
-    public function __construct(GoogleAuthenticatorInterface $googleAuthenticator)
+    public function __construct(GoogleAuthenticatorInterface $googleAuthenticator, KernelInterface $kernel)
     {
+        $this->projectDir = $kernel->getProjectDir();
         $this->googleAuthenticator = $googleAuthenticator;
     }
 
@@ -52,7 +62,7 @@ class SecurityController extends AbstractController
         EntityManagerInterface $entityManager
     ): Response {
         $user = $this->getUser();
-        $form = $this->createForm(AccountType::class, $user,['user'=>$user]);
+        $form = $this->createForm(AccountType::class, $user, ['user' => $user]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             if ($form['password']->getData()) {
@@ -63,19 +73,17 @@ class SecurityController extends AbstractController
             ) : $user->setGoogleAuthenticatorSecret(null);
             try {
                 $entityManager->flush();
-                $this->addFlash(
-                    'success',
-                    'Vos informations ont bien été mises à jour, votre clé est : ' . $this->googleAuthenticator->getQRContent(
-                        $user
-                    )
-                );
             } catch (\Exception $e) {
                 $this->addFlash('error', 'Une erreur est survenue lors de la mise à jour de vos informations');
             }
+            if ($user->isGoogleAuthenticatorEnabled()) {
+                $this->createQrCodesPNG($this->googleAuthenticator->getQRContent($user), $user);
+            }
+            return $this->redirectToRoute('app_account');
         }
         return $this->render('security/account.html.twig', [
             'form' => $form->createView(),
-            'QrCode' => $this->googleAuthenticator->getQRContent($user)
+            'QrCode' => $user->isGoogleAuthenticatorEnabled() ? '/qrCodes/' . $user->getId() . '.png' : null
         ]);
     }
 
@@ -83,6 +91,30 @@ class SecurityController extends AbstractController
     public function getGoogleAuth()
     {
         return $this->render('security/google_auth.html.twig');
+    }
+
+    private function createQrCodesPNG(string $qrCodeContent, User $user)
+    {
+        $builder = new Builder(
+            writer: new PngWriter(),
+            writerOptions: [],
+            validateResult: false,
+            data: $qrCodeContent,
+            encoding: new Encoding('UTF-8'),
+            errorCorrectionLevel: ErrorCorrectionLevel::High,
+            size: 400,
+            margin: 10,
+            roundBlockSizeMode: RoundBlockSizeMode::Margin,
+            labelText: 'Scan the code',
+            labelAlignment: LabelAlignment::Center
+        );
+        try {
+            $qrCode = $builder->build();
+            $filePath = $this->projectDir . '/public/qrCodes/' . $user->getId() . '.png';
+            $qrCode->saveToFile($filePath);
+        } catch (\Exception $e) {
+            dd($e);
+        }
     }
 
 }
